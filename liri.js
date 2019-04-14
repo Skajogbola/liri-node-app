358b9bf664b0e2ebba649fb342a12b289cdbab30
Simmie <skajogbola@users.noreply.github.com>
2019-04-14 16:10:11 -0400

    concert updates

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-// require("dotenv").config();
+require("dotenv").config();
 
 //link for OMDB API and the Bands In Town API
 var axios = require("axios");
@@ -13,8 +13,8 @@ var fs = require("fs");
 // var Spotify = require("node-spotify-api");
 // var spotify = new Spotify(keys.spotify);
 
-// // REQUIRE REQUEST
-// let request = require("request");
+// REQUIRE REQUEST
+let request = require("request");
 
 // // REQUIRE MOMENT
 // var moment = require('moment');
@@ -48,22 +48,51 @@ function command(userInput, userQuery) {
 command(userInput, userQuery);
 
 function movieThis() {
-    console.log("Searching for " + userQuery);
-    if(!userQuery) {
+    console.log("SEARCHING FOR MOVIE: " + userQuery);
+    if (!userQuery) {
         userQuery = "Mr. Nobody"
     }
-axios.get("http://www.omdbapi.com/?t=" +userQuery+ "&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    console.log("Title of the movie is: " + response.data.Title);
-    console.log("Year the movie came out: " + response.data.Year);
-    console.log("IMDB Rating of the movie is: " + response.data.imdbRating);
-    console.log("Rotten Tomatoes Rating of the movie is: " + response.data.Ratings[1].Value)
-    console.log("Country where the movie was produced is: " + response.data.Country);
-    console.log("Language of the movie is: " + response.data.Language);
-    console.log("Plot of the movie is: " + response.data.Plot);
-    console.log("Actors in the movie are: " + response.data.Actors);
-
-  }
-)
+    axios.get("http://www.omdbapi.com/?t=" + userQuery + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            console.log("Title of the movie is: " + response.data.Title);
+            console.log("Year the movie came out: " + response.data.Year);
+            console.log("IMDB Rating of the movie is: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating of the movie is: " + response.data.Ratings[1].Value)
+            console.log("Country where the movie was produced is: " + response.data.Country);
+            console.log("Language of the movie is: " + response.data.Language);
+            console.log("Plot of the movie is: " + response.data.Plot);
+            console.log("Actors in the movie are: " + response.data.Actors);
+        })
 }
 
+function concertThis() {
+    console.log("SEARCHING FOR CONCERT: " + userQuery);
+    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp", function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            let response = JSON.parse(body);
+            // PARSE DATA AND USE FOR LOOP TO ACCESS PATHS TO DATA
+            if (response.length > 0) {
+                for (i = 0; i < 4; i++) {
+            console.log("Name of the venue is: " + response[i].venue.name);
+            console.log("Venue location: " + `${response[i].venue.city}, ${response[i].venue.country}`);
+            // console.log("Date of the Event(MM/DD/YYYY): " + response.data.imdbRating);
+                }
+            }
+        }
+    });
+}
+
+// function concertThis() {
+//     console.log("SEARCHING FOR CONCERT: " + userQuery);
+// axios.get("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp").then(
+//         function (response) {
+//             // let response = JSON.parse(body);
+//             // PARSE DATA AND USE FOR LOOP TO ACCESS PATHS TO DATA
+//             if (response.length > 0) {
+//                 for (i = 0; i < 4; i++) {
+//             console.log("Name of the venue is: " + response[i].venue.name);
+//             console.log("Venue location: " + response[i].venue.city);
+//                 }
+//             } 
+//     })
+// }
