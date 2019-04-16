7540390d675589ff10e994ba3865a9ca3ea0398e
Simmie <skajogbola@users.noreply.github.com>
2019-04-15 20:52:42 -0400

    commit

--- liri.js
+++ liri.js
@@ -7,11 +7,15 @@ var axios = require("axios");
 var fs = require("fs");
 
 //Link for keys
-// var keys = require("./keys.js");
+var keys = require("./keys.js");
 
-// //Link for sportify
-// var Spotify = require("node-spotify-api");
+//Link for sportify
+var Spotify = require("node-spotify-api");
 // var spotify = new Spotify(keys.spotify);
+var spotify = new Spotify({
+    id: 'c1c1927486ea45999cfe4f25ea20951b',
+    secret: 'a0fdeda5c3664887bd192b8ac9871b79'
+});
 
 // REQUIRE REQUEST
 let request = require("request");
@@ -39,8 +43,8 @@ function command(userInput, userQuery) {
             movieThis();
             break;
 
-        case "do-this":
-            doThis(userQuery);
+        case "do-what-it-says":
+            doWhatItSays(userQuery);
             break;
     }
 }
@@ -84,5 +88,44 @@ function concertThis() {
         }
     });
 }
+
+function spotifyThisSong() {
+    console.log("SEARCHING FOR A SONG: " + userQuery);
+    if (!userQuery) {
+        userQuery = "The Sign ace of base"
+    }
+    spotify.search({
+        type: 'track',
+        query: userQuery,
+        limit: 1
+    }, function (error, data) {
+        if (error) {
+            return console.log('Error occurred: ' + error);
+        }
+        
+        var spotifyArray = data.tracks.items;
+        for (i = 0; i < spotifyArray.length; i++) {
+            console.log("The name of the Artist(s): " + spotifyArray[i].album.artists[0].name);
+            console.log("The song's name: " + spotifyArray[i].name);
+            console.log("A preview link of the song from Spotify: " + spotifyArray[i].external_urls.spotify);
+            console.log("The album that the song is from: " + spotifyArray[i].album.name);
+        };
+        })
+}
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+
+        var dataArr = data.split(",");
+        userInput = dataArr[0];
+        userQuery = dataArr[1];
+    
+        command(userInput, userQuery);
+    });
+};
+
 
 
